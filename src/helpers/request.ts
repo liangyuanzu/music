2ed72e03affb84ad7ce9489bd8bb86f64d90e23b
liangyuanzu <liangyuanzu@gmail.com>
2021-01-20 20:48:08 +0800

    feat(http): 网络工具类封装

---
+++ src/helpers/request.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+axios.interceptors.request.use(
+  (config: any) => config,
+  (error: any) => Promise.reject(error),
+);
+
+axios.interceptors.response.use(
+  (response: any) => response,
+  (err: any) => {
+    const error = err;
+    if (error && error.response) {
+      switch (error.response.status) {
+        case 403:
+          error.message = '拒绝访问';
+          break;
+        case 404:
+          error.message = '请求错误,未找到该资源';
+          break;
+        case 500:
+          error.message = error.response.data.message;
+          break;
+        default:
+          error.message = `连接错误${error.response.status}`;
+      }
+    } else {
+      error.message = '连接到服务器失败';
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+const $request = (url: string, Options?: any) => new Promise((resolve, reject) => {
+  axios({
+    baseURL: '/',
+    data: Options.data, // post 传参
+    method: Options.method || 'GET',
+    params: Options.params, // get 传参
+    url,
+    ...Options,
+  })
+    .then((res) => {
+      if (res.data.code === 200) {
+        resolve(res.data);
+      } else {
+        reject(res.data);
+      }
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+export default $request;
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,30 @@
 module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': '',
+        },
+      },
+    },
+  },
   configureWebpack: {
     module: {
-      rules: [{
-        test: /\.(html)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'html-loader',
-          options: {
-            minimize: true,
+      rules: [
+        {
+          test: /\.(html)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
           },
         },
-      }],
+      ],
     },
   },
 };
