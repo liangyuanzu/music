fb4162885bc4ee05ee24f755df5e4bcaf70555d8
liangyuanzu <liangyuanzu@gmail.com>
2021-01-20 15:22:06 +0800

    fix: 修复 Expected linebreaks to be 'LF' but found 'CRLF'.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': 0,
   },
 };
