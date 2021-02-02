08d4bf4bc012b31b952f2c8f54818e4c7e6bb6a3
liangyuanzu <liangyuanzu@gmail.com>
2021-02-02 12:15:11 +0800

    feat(normal-player): 默认播放界面

---
+++ src/components/Player/NormalPlayer.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="normal-player">
+    <div class="player-warpper">
+      <PlayerHeader></PlayerHeader>
+      <PlayerMiddle></PlayerMiddle>
+      <PlayerBottom></PlayerBottom>
+    </div>
+    <div class="player-bg">
+      <img
+        src="https://p1.music.126.net/-EHFGXVwLwy7ra48lDKMfg==/109951165611159240.jpg"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+import PlayerHeader from './PlayerHeader.vue';
+import PlayerMiddle from './PlayerMiddle.vue';
+import PlayerBottom from './PlayerBottom.vue';
+
+@Component({
+  name: 'NormalPlayer',
+  components: {
+    PlayerHeader,
+    PlayerMiddle,
+    PlayerBottom,
+  },
+})
+export default class NormalPlayer extends Vue {}
+</script>
+
+<style lang='scss' scoped>
+@import '@/assets/css/variable';
+@import '@/assets/css/mixin';
+
+.normal-player {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+  @include bg_sub_color();
+
+  .player-warpper {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    z-index: 9999;
+  }
+  .player-bg {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    img {
+      height: 100%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      filter: blur(10px);
+    }
+  }
+}
+</style>
--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,36 +1,21 @@
 <template>
-  <div class="player">
-    <PlayerHeader></PlayerHeader>
-    <PlayerMiddle></PlayerMiddle>
-    <PlayerBottom></PlayerBottom>
+  <div>
+    <NormalPlayer></NormalPlayer>
   </div>
 </template>
 
 <script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator';
-import PlayerHeader from '../components/Player/PlayerHeader.vue';
-import PlayerMiddle from '../components/Player/PlayerMiddle.vue';
-import PlayerBottom from '../components/Player/PlayerBottom.vue';
+import NormalPlayer from '../components/Player/NormalPlayer.vue';
 
 @Component({
   name: 'Player',
   components: {
-    PlayerHeader,
-    PlayerMiddle,
-    PlayerBottom,
+    NormalPlayer,
   },
 })
 export default class Player extends Vue {}
 </script>
 
 <style lang='scss' scoped>
-.player {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: pink;
-  z-index: 999;
-}
 </style>
