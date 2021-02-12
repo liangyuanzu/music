d2757884d944f281016f0088dc50af120d64b4ce
liangyuanzu <liangyuanzu@gmail.com>
2021-02-12 21:45:10 +0800

    feat(play): 播放功能

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -3,12 +3,12 @@
     <NormalPlayer></NormalPlayer>
     <MiniPlayer></MiniPlayer>
     <ListPlayer></ListPlayer>
-    <audio :src="currentSong.url"></audio>
+    <audio :src="currentSong.url" ref="audio"></audio>
   </div>
 </template>
 
 <script lang='ts'>
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
 import { Getter } from 'vuex-class';
 import NormalPlayer from '../components/Player/NormalPlayer.vue';
 import MiniPlayer from '../components/Player/MiniPlayer.vue';
@@ -24,6 +24,30 @@ import ListPlayer from '../components/Player/ListPlayer.vue';
 })
 export default class Player extends Vue {
   @Getter('currentSong') currentSong;
+
+  @Getter('isPlaying') isPlaying;
+
+  @Getter('currentIndex') currentIndex;
+
+  @Watch('isPlaying')
+  onIsPlayingChanged(val: boolean) {
+    if (val) {
+      (this.$refs.audio as any).play();
+    } else {
+      (this.$refs.audio as any).pause();
+    }
+  }
+
+  @Watch('currentIndex')
+  onCurrentIndexChanged() {
+    (this.$refs.audio as any).oncanplay = () => {
+      if (this.isPlaying) {
+        (this.$refs.audio as any).play();
+      } else {
+        (this.$refs.audio as any).pause();
+      }
+    };
+  }
 }
 </script>
 
