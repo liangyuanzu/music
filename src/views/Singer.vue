715c247dd94ca0ab85a89b612ab3af622d4ba4b4
liangyuanzu <liangyuanzu@gmail.com>
2021-02-17 13:22:05 +0800

    feat(singer-style): 歌手列表样式

--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>歌手</div>
+  <div class="singer">
+    <div class="singer-wrapper">
+      <ScrollView>
+        <ul class="list-wrapper">
+          <li
+            class="list-group"
+            v-for="(item, index) in artistList"
+            :key="index"
+          >
+            <h2 class="group-title">{{ letters[index] }}</h2>
+            <ul>
+              <li
+                class="group-item"
+                v-for="artist in artistList[index]"
+                :key="artist.id"
+              >
+                <img v-lazy="artist.img1v1Url" alt="" />
+                <p>{{ artist.name }}</p>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </ScrollView>
+    </div>
+  </div>
 </template>
 
 <script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator';
 import { Getter, Action } from 'vuex-class';
+import ScrollView from '../components/ScrollView.vue';
 
 @Component({
   name: 'Singer',
+  components: {
+    ScrollView,
+  },
 })
 export default class Singer extends Vue {
   get letters() {
@@ -38,4 +66,50 @@ export default class Singer extends Vue {
 </script>
 
 <style lang='scss' scoped>
+@import '@/assets/css/variable';
+@import '@/assets/css/mixin';
+
+.singer {
+  width: 100%;
+  height: 100%;
+  .singer-wrapper {
+    position: fixed;
+    top: 184px;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+    @include bg_sub_color();
+    .list-wrapper {
+      .list-group {
+        .group-title {
+          @include bg_color();
+          @include font_size($font_medium);
+          color: #fff;
+          padding: 10px 20px;
+          box-sizing: border-box;
+        }
+        .group-item {
+          display: flex;
+          justify-content: flex-start;
+          padding: 10px 20px;
+          border-bottom: 1px solid #ccc;
+          img {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            overflow: hidden;
+          }
+          p {
+            @include font_size($font_medium);
+            @include font_color();
+            display: flex;
+            align-items: center;
+            margin-left: 20px;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
